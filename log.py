1658254cbedfd2e4ab1a7e3ef302e7b956ebca3b
chryoung <29620734+chryoung@users.noreply.github.com>
2024-09-02 12:31:37 +0800

    Add file log sink

--- log.py
+++ log.py
@@ -2,9 +2,14 @@ import logging
 
 def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
+
     stream = logging.StreamHandler()
     formatter = logging.Formatter('[%(asctime)s][%(name)s][%(levelname)s]%(message)s')
     stream.setFormatter(formatter)
     logger.addHandler(stream)
+
+    file = logging.FileHandler("FileSense.log")
+    file.setFormatter(formatter)
+    logger.addHandler(file)
 
     return logger
